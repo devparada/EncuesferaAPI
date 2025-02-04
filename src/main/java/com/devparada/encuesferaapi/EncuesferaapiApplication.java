c88a3d5d66d5cce31528a68fae2131b8c6401bbb
Raúl <raul.parada040103@gmail.com>
2025-02-04 13:48:08 +0100

    Incializado EncuesferaAPI

---
+++ src/main/java/com/devparada/encuesferaapi/EncuesferaapiApplication.java
@@ -0,0 +1,13 @@
+package com.devparada.encuesferaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncuesferaapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncuesferaapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devparada/encuesferaapi/EncuesferaapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devparada.encuesferaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncuesferaapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
